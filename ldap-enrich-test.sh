3350c04e881194c6275ee1289e83b3fab2f2f695
Raymond Doetjes <rdoetjes@phonax.com>
2020-12-17 08:17:43 +0100

    typo in README

--- ldap-enrich-test.sh
+++ ldap-enrich-test.sh
@@ -3,13 +3,14 @@
 		--sBind="cn=admin,dc=phonax" \
 		--sPass="Phonax01" \
         --sBaseDN="ou=source,ou=users,dc=phonax" \
-		--sAttribs="loginShell, description, street, postalCode" \
+		--sAttribs="loginShell, description" \
 		--sFilter="(&(objectClass=posixAccount)(cn=user1))" \
 		--dLdap=ldap://127.0.0.1 \
 		--dBind="cn=admin,dc=phonax" \
 		--dPass="Phonax01" \
         --dBaseDN="ou=dest,ou=users,dc=phonax" \
-		--dAttribs="loginShell, description, street, postalCode" \
+		--dAttribs="loginShell, description" \
 		--dFilter="(&(objectClass=posixAccount)(cn=*))" \
         --simulate \
-        #--replace
+        #--replace \
+
